3ba68b8826ca1252b71056f95885ce5510a1e551
Akash Suttakoti <asuttakoti@gmail.com>
2025-12-03 19:34:16 +0530

    Update ProtectedRoute.js

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -4,7 +4,6 @@ export default function ProtectedRoute({ children }) {
   const { token } = React.useContext(AuthContext);
 
   if (!token) {
-    // User is not logged in → redirect to login page
     window.location.href = "/login";
     return null;
   }
