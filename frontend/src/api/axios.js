12db3292fd3473da1aac1039006be42e088acd5b
Akash Suttakoti <asuttakoti@gmail.com>
2025-12-03 19:33:15 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,7 @@
-// Axios instance configured for backend API
 const api = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
-// Attach Authorization header automatically
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
