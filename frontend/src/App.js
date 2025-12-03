94efa3998818ee31b996d935fb78ed3f791661da
Akash Suttakoti <asuttakoti@gmail.com>
2025-12-03 19:21:24 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
-export default function App() {
+import LoginPage from "./pages/LoginPage.js";
+import Dashboard from "./pages/Dashboard.js";
+import PatientSearch from "./pages/PatientSearch.js";
+import PatientProfile from "./pages/PatientProfile.js";
+import PatientForm from "./pages/PatientForm.js";
+import { AuthProvider } from "./context/AuthContext.js";
+
+const { Routes, Route, Navigate } = ReactRouterDOM;
+
+function App() {
   return (
-    <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>EHR Application</h1>
-      <p>Frontend setup successful.</p>
-    </div>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/patients" element={<PatientSearch />} />
+        <Route path="/patients/new" element={<PatientForm />} />
+        <Route path="/patients/:id" element={<PatientProfile />} />
+      </Routes>
+    </AuthProvider>
   );
 }
+
+export default App;
