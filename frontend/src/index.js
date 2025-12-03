5ff5f45e55cd50775dd0c664a5ea37e43c0335cc
Akash Suttakoti <asuttakoti@gmail.com>
2025-12-03 19:16:22 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,9 @@
+// Import App component (browser-only import)
+import App from "./App.js";
+
 const { BrowserRouter } = ReactRouterDOM;
 
-function App() {
-  return (
-    <div>
-      <h1>EHR Frontend Working</h1>
-      <p>This is the starting point.</p>
-    </div>
-  );
-}
-
+// React root render
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
