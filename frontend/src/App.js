ecef48f2fc49e9fe5164b57fd74e6680dcb4800c
Akash Suttakoti <asuttakoti@gmail.com>
2025-12-03 19:16:49 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>EHR Application</h1>
+      <p>Frontend setup successful.</p>
+    </div>
+  );
+}
