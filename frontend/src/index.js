b8bbd848a40a92b5de8843c6131bb1eceee26c39
Akash Suttakoti <asuttakoti@gmail.com>
2025-12-03 19:24:24 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,7 @@
-// Import App component (browser-only import)
 import App from "./App.js";
 
 const { BrowserRouter } = ReactRouterDOM;
 
-// React root render
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
