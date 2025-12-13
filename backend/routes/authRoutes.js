1f708aa57589a5d2380243882f6bc33dd8021160
Akash Suttakoti <asuttakoti@gmail.com>
2025-12-13 19:59:19 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,14 +1,8 @@
 // routes/authRoutes.js
-
 const express = require('express');
 const router = express.Router();
 const { login } = require('../controllers/authController');
 
-// --------------------
-// AUTH ROUTES
-// --------------------
-
-// Login only (simple, dummy auth)
 router.post('/login', login);
 
 module.exports = router;
