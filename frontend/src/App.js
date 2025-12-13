50bcc701cfe7b42a5d7cca9132d87b1cbb0da2ac
Akash Suttakoti <asuttakoti@gmail.com>
2025-12-13 12:47:41 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,64 +1,85 @@
-import Navbar from "./components/Navbar.js";
-import ProtectedRoute from "./components/ProtectedRoute.js";
+// src/App.js
 
-import LoginPage from "./pages/LoginPage.js";
-import Dashboard from "./pages/Dashboard.js";
-import PatientSearch from "./pages/PatientSearch.js";
-import PatientProfile from "./pages/PatientProfile.js";
-import PatientForm from "./pages/PatientForm.js";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-import { AuthProvider } from "./context/AuthContext.js";
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import Navbar from './components/Navbar';
 
-const { Routes, Route, Navigate } = ReactRouterDOM;
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import PatientList from './pages/PatientList';
+import PatientForm from './pages/PatientForm';
+import PatientProfile from './pages/PatientProfile';
 
-export default function App() {
+import './index.css';
+
+function App() {
   return (
     <AuthProvider>
-      <Navbar />
+      <Router>
+        <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <div className="container">
+          <Routes>
+            {/* Public */}
+            <Route path="/login" element={<Login />} />
 
-        {/* Public */}
-        <Route path="/login" element={<LoginPage />} />
+            {/* Default */}
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Navigate to="/dashboard" />
+                </ProtectedRoute>
+              }
+            />
 
-        {/* Protected */}
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+            {/* Protected */}
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
 
-        <Route
-          path="/patients"
-          element={
-            <ProtectedRoute>
-              <PatientSearch />
-            </ProtectedRoute>
-          }
-        />
+            <Route
+              path="/patients"
+              element={
+                <ProtectedRoute>
+                  <PatientList />
+                </ProtectedRoute>
+              }
+            />
 
-        <Route
-          path="/patients/new"
-          element={
-            <ProtectedRoute>
-              <PatientForm />
-            </ProtectedRoute>
-          }
-        />
+            <Route
+              path="/patients/new"
+              element={
+                <ProtectedRoute>
+                  <PatientForm />
+                </ProtectedRoute>
+              }
+            />
 
-        <Route
-          path="/patients/:id"
-          element={
-            <ProtectedRoute>
-              <PatientProfile />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+            <Route
+              path="/patients/:id"
+              element={
+                <ProtectedRoute>
+                  <PatientProfile />
+                </ProtectedRoute>
+              }
+            />
+
+            {/* Fallback */}
+            <Route path="*" element={<Navigate to="/dashboard" />} />
+          </Routes>
+        </div>
+      </Router>
     </AuthProvider>
   );
 }
+
+export default App;
