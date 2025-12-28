f419396e937dde6821393c8936f7ec91794af146
Akash Suttakoti <asuttakoti@gmail.com>
2025-12-28 18:41:53 +0530

    Update AuthContext.js

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -17,7 +17,7 @@ export const AuthProvider = ({ children }) => {
       setLoading(true);
 
       const res = await axios.post(
-        'http://13.233.120.140:3001/api/auth/login',
+        'http://143.205.115.134:3001/api/auth/login',
         credentials,
         {
           headers: {
