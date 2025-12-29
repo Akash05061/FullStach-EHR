c7e22f236beb2d63a638d67b075c5c5a341c801d
Akash Suttakoti <asuttakoti@gmail.com>
2025-12-29 12:12:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const authRoutes = require('./routes/authRoutes');
+const patientRoutes = require('./routes/patientRoutes');
 
 const app = express();
 const PORT = 3001;
@@ -11,6 +12,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/auth', authRoutes);
+app.use('/api/patients', patientRoutes);
 
 // health check
 app.get('/api/health', (req, res) => {
