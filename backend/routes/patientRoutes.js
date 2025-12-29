ac3f32963792bfa09dca9c7a88c68f0d612aa4bc
Akash Suttakoti <asuttakoti@gmail.com>
2025-12-29 12:12:56 +0530

    Update patientRoutes.js

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/authMiddleware');
+
 const {
   getAllPatients,
   getPatientById,
   createPatient
 } = require('../controllers/patientController');
 
-router.get('/', getAllPatients);
-router.get('/:id', getPatientById);
-router.post('/', createPatient);
+router.get('/', auth, getAllPatients);
+router.get('/:id', auth, getPatientById);
+router.post('/', auth, createPatient);
 
 module.exports = router;
