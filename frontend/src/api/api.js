187fdcb4597309995cc2f15329389996dcdc2ca8
Akash Suttakoti <asuttakoti@gmail.com>
2026-01-07 13:31:37 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -5,7 +5,7 @@ import axios from 'axios';
  * Token is injected from localStorage
  */
 const api = axios.create({
-  baseURL: 'http://65.0.103.216:3001/api'
+  baseURL: 'http://43.204.211.241:3001/api'
 });
 
 // -----------------------------
